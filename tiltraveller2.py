f2f9a879262b5adc40333e6a2dda8ae9ab47c440
emiliabergmann <emiliah19@ru.is>
2020-10-08 10:05:19 +0000

    step 2

--- tiltraveller2.py
+++ tiltraveller2.py
@@ -47,57 +47,65 @@ x = STARTING_X
 y = STARTING_Y
 coins = 0
 
+def play(x,y,coins):
+    while True:
+        not_again = True
+        print('You can travel: ', end='')
+        n = north(x, y)
+        e = east(x, y)
+        s = south(x, y)
+        w = west(x, y)
+        
+        if n == True and e == False and s == False and w == False:
+            print('(N)orth', end='')
+        if n == True and e == True and s == True and w == False:
+            print('(N)orth or (E)ast or (S)outh', end='')
+        if e == True and s == True and n == False and w == False:
+            print('(E)ast or (S)outh', end='')
+        if e == True and w == True and n == False and s == False:
+            print('(E)ast or (W)est', end='')
+        if s == True and w == True and n == False and e == False:
+            print('(S)outh or (W)est', end='')
+        if n == True and s == True and w == False and e == False:
+            print('(N)orth or (S)outh', end='')        
+        print('.', end='')
+        print()
+        direction = input('Direction: ')
+        direction = direction.lower()
+        if direction == 'n' or direction == 'e' or direction == 's' or direction == 'w':
+            if direction == 'n' and n == True:
+                y += 1
+            elif direction == 'e' and e == True:
+                x += 1 
+            elif direction == 's' and s == True:
+                y -= 1
+            elif direction == 'w' and w == True:
+                x -= 1
+            else: 
+                not_again = False
+                print('Not a valid direction!')
 
-while True:
-    not_again = True
-    print('You can travel: ', end='')
-    n = north(x, y)
-    e = east(x, y)
-    s = south(x, y)
-    w = west(x, y)
-    
-    if n == True and e == False and s == False and w == False:
-        print('(N)orth', end='')
-    if n == True and e == True and s == True and w == False:
-        print('(N)orth or (E)ast or (S)outh', end='')
-    if e == True and s == True and n == False and w == False:
-        print('(E)ast or (S)outh', end='')
-    if e == True and w == True and n == False and s == False:
-        print('(E)ast or (W)est', end='')
-    if s == True and w == True and n == False and e == False:
-        print('(S)outh or (W)est', end='')
-    if n == True and s == True and w == False and e == False:
-        print('(N)orth or (S)outh', end='')        
-    print('.', end='')
-    print()
-    direction = input('Direction: ')
-    direction = direction.lower()
-    if direction == 'n' or direction == 'e' or direction == 's' or direction == 'w':
-        if direction == 'n' and n == True:
-             y += 1
-        elif direction == 'e' and e == True:
-             x += 1 
-        elif direction == 's' and s == True:
-             y -= 1
-        elif direction == 'w' and w == True:
-             x -= 1
-        else: 
-            not_again = False
+        else:
             print('Not a valid direction!')
+            not_again = False
 
-    else:
-        print('Not a valid direction!')
-        not_again = False
-
-    if not_again:
-        if x == 1 and y == 2 or x == 2 and y == 2 or x == 3 and y == 2:
-            coins = pulling_lever(coins)
+        if not_again:
+            if x == 1 and y == 2 or x == 2 and y == 2 or x == 2 and y == 3 or x == 3 and y == 2:
+                coins = pulling_lever(coins)
 
 
+        if x == 3 and y == 1:
+            print('Victory!', end=' ')
+            print('Total coins {}.'.format(coins))
+            break
+    return 
 
+def main():
+    play(x,y,coins)
+    choice = input("Play again (y/n): ")
+    while choice == "y" or choice == "Y":
+        play(x,y,coins)
+        choice = input("Play again (y/n): ")
+    return 
 
-    if x == 3 and y == 1:
-        print('Victory!', end=' ')
-        print('Total coins {}.'.format(coins))
-        break
-
+main()
